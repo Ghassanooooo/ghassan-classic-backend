8b85b3d046af6f3bba12b3359c3c7432ae3a8220
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2019-10-05 20:08:57 +0200

    added html-pdf pakege and achived gen a basic pdf file with simple styles

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+
+const fs = require("fs");
+const path = require("path");
+const pdf = require("html-pdf");
+const ejs = require("ejs");
+const router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Ghassan classic" });
+});
+
+router.get("/test", function(req, res, next) {
+  fs.readFile(
+    path.join(__dirname, "..", "documents", "data", "workoutData.json"),
+    (err, data) => {
+      let workoutJsonData = JSON.parse(data);
+      console.log(workoutJsonData);
+    }
+  );
+});
+
+router.get("/download", function(req, res, next) {
+  fs.readFile(
+    path.join(__dirname, "..", "documents", "data", "workoutData.json"),
+    (err, dataJson) => {
+      let workoutJsonData = JSON.parse(dataJson);
+      console.log(workoutJsonData);
+      ejs.renderFile(
+        path.join(__dirname, "..", "views", "index.ejs"),
+        workoutJsonData,
+        (err, data) => {
+          let options = {
+            height: "11.25in",
+            width: "8.5in",
+            header: {
+              height: "1mm"
+            },
+            footer: {
+              height: "1mm"
+            }
+          };
+          pdf.create(data, options).toFile("fitness-program.pdf", err => {
+            res.sendFile(path.join(__dirname, "..", "fitness-program.pdf"));
+          });
+        }
+      );
+    }
+  );
 });
 
 module.exports = router;
